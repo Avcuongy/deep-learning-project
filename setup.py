17f07e314362a345526d70918ec7ee22cb2a5c12
Thiện Nguyễn <thiennguyen03001@gmail.com>
2026-06-09 20:43:20 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,1 @@
-from setuptools import setup, find_packages
 
-setup(
-    name="deep-learning-project",
-    version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    python_requires=">=3.9",
-)
